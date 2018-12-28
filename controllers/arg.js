3d424755ebb149bc8161cfbedde4958b1b9fbcc4
Jean-Loup MONNIER <jloup.m@gmail.com>
2018-12-28 12:57:10 +0100

    try to fix time display

--- controllers/arg.js
+++ controllers/arg.js
@@ -11,7 +11,7 @@ function index (req, res) {
 			dataEnigma.push({
 				name: e[i].name,
 				id: e[i].id,
-				first_time_visited: e[i].first_time_visited != null ? e[i].first_time_visited.toLocaleDateString('fr-FR') + ' ' + e[i].first_time_visited.toLocaleTimeString() : '-',
+				first_time_visited: e[i].first_time_visited != null ? e[i].first_time_visited.toLocaleDateString('fr-FR') + ' ' + e[i].first_time_visited.toLocaleTimeString('fr-FR') : '-',
 				remaining: e[i].time_before_hint,
 				hold_update: e[i].first_time_visited == null || e[i].time_before_hint.remaining === 0,
 				symbole: e[i].first_time_visited == null ? '-' : 'Done'
@@ -24,7 +24,7 @@ function index (req, res) {
 					dataWinner.push({
 						id_enigma: w[i].enigma_id,
 						name: w[i].name,
-						date: w[i].date.toLocaleDateString('fr-FR') + ' ' + w[i].date.toLocaleTimeString()
+						date: w[i].date.toLocaleDateString('fr-FR') + ' ' + w[i].date.toLocaleTimeString('fr-FR')
 					});
 				}
 				res.render('analytics', {enigmas: dataEnigma, winners: dataWinner, pending: c, layout: false});
