3849588e712e13f38d8269482937f5d8e1e39742
Jean-Loup MONNIER <jloup.m@gmail.com>
2019-01-02 16:28:01 +0100

    enigma : morse 1led / lettre + multiple color

--- public/js/custom/log5.js
+++ public/js/custom/log5.js
@@ -2,34 +2,64 @@
 /* global $:true alert:true */
 
 function blinkLedRecursive (led, sequence, index) {
-	let ledDelay = 500;
-	if (led.style.display === '') {
-		led.style.display = 'none';
+	let ledDelay = 700;
+	if ($(led).is(':visible')) {
+		$(led).hide();
 		if (index >= sequence.length) {
 			index = 0;
 			ledDelay = 5000;
 		}
 	} else {
-		led.style.display = '';
+		$(led).show();
 		if (sequence[index++] < 50) ledDelay = 250;
 	}
 	setTimeout(blinkLedRecursive.bind(null, led, sequence, index), ledDelay);
 }
 
 $(document).ready(function () {
-	// alert('custom js');
-	let greenLed1 = document.getElementById('green-led-1');
-	greenLed1.style.display = 'none';
+	let color = ['green', 'orange', 'red', 'purple', 'blue'];
+	let ledSequence = [ [ 83, 6 ],
+	[ 48, 72 ],
+	[ 78, 83 ],
+	[],
+	[ 36 ],
+	[ 81 ],
+	[],
+	[ 19, 11 ],
+	[ 27, 74, 70, 31 ],
+	[ 42, 16, 25 ],
+	[ 35, 61 ],
+	[],
+	[ 45, 21, 6 ],
+	[ 75, 40, 83, 14 ],
+	[ 0, 19 ],
+	[ 36 ],
+	[ 63, 14 ],
+	[ 76 ],
+	[ 24, 47 ],
+	[ 37, 52 ],
+	[],
+	[ 44, 62, 97, 20 ],
+	[ 79, 51, 68 ],
+	[ 99 ],
+	[ 41 ],
+	[ 4, 28, 45 ],
+	[ 66 ],
+	[ 26, 98 ],
+	[ 44, 25, 48 ],
+	[],
+	[ 12 ],
+	[ 29, 42, 26 ],
+	[ 72 ] ];
 
-	// string are not uglyfied
-	let greenLed1Sequence = [60, 10, 17, 28, 60, 87, 54, 98, 23, 12, 90, 98, 67, 54, 57, 76, 14, 35, 87, 2, 99, 6];
-
-	blinkLedRecursive(greenLed1, greenLed1Sequence, 0);
-
-	let greenLed2 = document.getElementById('green-led-2');
-	greenLed2.style.display = 'none';
-
-	let greenLed2Sequence = [76, 65, 98, 67, 11, 13];
-
-	blinkLedRecursive(greenLed2, greenLed2Sequence, 0);
+	for (let i = 0; i < ledSequence.length; i++) {
+		var randColor = color[Math.floor(Math.random() * color.length)];
+		$('#fill').append(`<div class="arg-led-container"><div class="arg-led-border arg-dark-` + randColor + `"><div id="green-led-` + i + `" class="arg-led arg-` + randColor + `"></div></div></div>`);
+		setTimeout(() => {
+			$('#green-led-' + i).hide();
+			if (ledSequence[i].length !== 0) {
+				blinkLedRecursive($('#green-led-' + i), ledSequence[i], 0);
+			}
+		}, 100);
+	}
 });
--- utils/morseToArray.js
+++ utils/morseToArray.js
@@ -1,27 +1,30 @@
 const letters = [ ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ];
 const morseLetters = [ '\xa0\xa0\xa0\xa0', '. _', '_ . . .', '_ . _ .', '_ . .', '.', '. . _ .', '_ _ .', '. . . .', '. .', '. _ _ _', '_ . _', '. _ . .', '_ _', '_ .', '_ _ _', '. _ _ .', '_ _ . _', '. _ .', '. . .', '_', '. . _', '. . . _', '. _ _', '_ . . _', '_ . _ _', '_ _ . .', '. _ _ _ _', '. . _ _ _', '. . . _ _', '. . . . _', '. . . . .', '_ . . . .', '_ _ . . .', '_ _ _ . .', '_ _ _ _ .', '_ _ _ _ _' ];
 
-var textToChange = 'test';
+var textToChange = 'Nam et ipsa scientia potestas est';
 
-var newText = '';
+var newText = [];
 for (var i = 0; i < textToChange.length; i++) {
 	for (var j = 0; j < 37; j++) {
 		if (textToChange[i].toLowerCase() === letters[j]) {
-			newText += morseLetters[j];
-			newText += '\xa0\xa0\xa0';
+			newText.push(morseLetters[j].replace(/\s+/g, '').split(''));
 			break;
 		}
 	}
 }
 console.log(newText);
-var morse = newText.replace(/\s+/g, '').split('');
+
 var result = [];
 
-for (var y = 0; y < morse.length; y++) {
-	if (morse[y] === '.') {
-		result.push(Math.floor(Math.random() * 50));
-	} else {
-		result.push(Math.floor(Math.random() * 50) + 50);
+for (var y = 0; y < newText.length; y++) {
+	var lettre = [];
+	for (var z = 0; z < newText[y].length; z++) {
+		if (newText[y][z] === '.') {
+			lettre.push(Math.floor(Math.random() * 50));
+		} else if (newText[y][z] === '_') {
+			lettre.push(Math.floor(Math.random() * 50) + 50);
+		}
 	}
+	result.push(lettre);
 }
 console.log(result);
