e3e9d70bbfe024a4b29388e0a4ee56993a3a660b
Kloven <jloup.m@gmail.com>
2019-08-26 22:22:32 +0200

    adding dotenv for development

--- config/config.js
+++ config/config.js
@@ -7,4 +7,10 @@ config.limiter = {
 	max: 60 // limit each IP to 100 requests per windowMs
 };
 config.limitMessage = 'Too many requests for this ip, pls stop :( </br> If you\'re not trying to DDoS,  please try again later. <3';
+
+// loading .env if on developement
+if (config.env === 'development') {
+	require('dotenv').config();
+	console.log('coucoucazdkjvnmekvnmevnmefnvmkefnvekfnvmj');
+}
 module.exports = config;
