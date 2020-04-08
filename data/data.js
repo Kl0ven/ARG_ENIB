c7456472c893ded3b94fdb6f078a4eac90965640
Kloven <jloup.m@gmail.com>
2020-04-08 12:47:04 +0200

    cleaning old enigmas

---
+++ data/data.js
@@ -0,0 +1,18 @@
+/* eslint-disable max-len */
+module.exports = [
+    {
+        name: 'Le compte est bon',
+        enigma_text: 'Utilisez tout !',
+        url: '47ff7827ce0982e459735c3c857d7bb5', // Le compte est bon
+        hint: '(((...)^...)/...)-...',
+        end_text: 'Bravo pour avoir terminé ces tests élémentaires. Dépêchons-nous, passons à la suite !',
+        type: 'eval',
+        flag: '42',
+        caracter: ['(', '(', '(', '5', '!', '-', '10', '5', ')', '^', '3', ')', '/', '75', ')', '-', '3'],
+        first_time_visited: null,
+        delay_to_hint: 5,
+        custom_js: 'le_compte_est_bon.js',
+        custom_html: 'le_compte_est_bon',
+        custom_css: 'le_compte_est_bon.css'
+    }
+];
